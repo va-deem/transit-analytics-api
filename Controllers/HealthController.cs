305ba59da6c207b2fc378990cb517b5934714d7e
Vadim S <20091002+va-deem@users.noreply.github.com>
2026-03-15 14:50:06 +1300

    Add initial structure

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TransitAnalyticsAPI.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "ok" });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -14,7 +14,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/health", () => Results.Ok(new { status = "ok" }))
-    .WithName("GetHealth");
+app.MapControllers();
 
 app.Run();
